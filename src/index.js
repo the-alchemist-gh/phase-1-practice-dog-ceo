40b55a9135e57dee1611a30a8583cc501e929426
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-06-23 23:13:32 +0000

    Added list for challenge 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 console.log('%c HI', 'color: firebrick')
+
+
+function getImg(){
+  
+  fetch('https://dog.ceo/api/breeds/image/random/4')
+  .then(resp=>resp.json())
+  .then(
+    (res)=>{
+      let dogImage = document.querySelector('#dog-image-container');
+      res.message.forEach((data) => {
+      // let dogImage = document.querySelector('#dog-image-container');
+      imgEl = document.createElement('img');
+      imgEl.src = data;
+      dogImage.appendChild(imgEl);
+      // dogImage. = `<img src="${imgEl}">`
+    })
+  })
+}
+
+function getBreed(){
+  
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(resp=>resp.json())
+  .then(
+    (res)=>{
+      let dogBreeds = document.querySelector('#dog-breeds');
+      console.log(res.message)
+      for (const data in res.message) {
+        console.log(data)
+        breedList = document.createElement('li');
+        breedList.textContent = data;
+        dogBreeds.appendChild(breedList);
+      }
+  })
+}
+getBreed()
+getImg()
