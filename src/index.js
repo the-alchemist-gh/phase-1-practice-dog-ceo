164f2459f3d4e2ce3e3fe8276c764f124a1924a0
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-06-24 10:03:48 +0000

    Completed challenge 4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 console.log('%c HI', 'color: firebrick')
 
+// Filter according to letter
+function changeBreed(){
+  let breedDropdown = document.getElementById('breed-dropdown');
+  
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(resp=>resp.json())
+  .then(
+    (res)=>{
+      let dogBreeds = document.querySelector('#dog-breeds');
+      dogBreeds.innerHTML='';
+      let dogBreedArr = Object.keys(res.message);
+      console.log(dogBreedArr)
+      
+      const filtered = dogBreedArr.filter(breedValue=>breedValue.charAt(0) === breedDropdown.value);
+      filtered.forEach(breed=>{
+        let breedList = document.createElement('li');
+        breedList.textContent = breed;
+        
+        dogBreeds.append(breedList);
+         // click to change color
+         breedList.addEventListener('click',()=>{
+          breedList.style.color = 'red';
+        })
+      })
+  })
+}
+
+// function filterBreed(breedValue){
+//   return 
+//   // console.log()
+// }
 
 function getImg(){
   
@@ -25,15 +56,19 @@ function getBreed(){
   .then(
     (res)=>{
       let dogBreeds = document.querySelector('#dog-breeds');
-      console.log(res.message)
+      // console.log(res.message)
       for (const data in res.message) {
-        console.log(data)
+        // console.log(data.filter(data.charAt(0)=== 'a'))
         let breedList = document.createElement('li');
         breedList.textContent = data;
         dogBreeds.appendChild(breedList);
+
+        // click to change color
         breedList.addEventListener('click',()=>{
           breedList.style.color = 'red';
         })
+
+
       }
   })
 }
